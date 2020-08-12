67a99d5910e9c3efe992900c74641cd3bc137883
Abdullahi Temidayo Jimoh <abdullahij951@gmail.com>
2020-08-12 17:35:25 +0100

    building features included

---
+++ admin/buildingform.php
@@ -0,0 +1,36 @@
+<?php
+        session_start();
+     
+        error_reporting(0);
+        $error="";
+        $msg="";
+        include('includes/config.php');
+        if(strlen($_SESSION['alogin'])==0)
+            {	
+        header('location:index.php');
+        }
+        else{
+         	
+if(isset($_POST['submit']))
+{
+
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+    $size = $_POST['size'];
+    $location = $_POST['location'];
+    $category = $_POST['category'];
+    echo $description;
+	
+    $sql="INSERT INTO `building`(`name`, `description`, `size`, `location`, `category`) VALUES (:name, :description, :size, :location, :category)";
+    $query = $dbh->prepare($sql);
+    $query-> bindParam(':name', $name, PDO::PARAM_STR);
+    $query-> bindParam(':description', $description, PDO::PARAM_STR);
+    $query-> bindParam(':size', $size, PDO::PARAM_STR);
+    $query-> bindParam(':location', $location, PDO::PARAM_STR);
+    $query-> bindParam(':category', $category, PDO::PARAM_STR);
+    $query->execute(); 
+    $msg="Rent Updated Successfully";
+    header('location:buildinglist.php');
+}
+}  
+?>
