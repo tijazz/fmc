e1a355f6892a23395db939cef80bd9fa353d5033
Abdullahi Temidayo Jimoh <abdullahij951@gmail.com>
2020-08-29 23:48:32 +0100

    Create salaryview.php

---
+++ admin/salaryview.php
@@ -0,0 +1,246 @@
+<?php
+        session_start();
+     
+        error_reporting(0);
+        $error="";
+        $msg="";
+        include('includes/config.php');
+        if(strlen($_SESSION['alogin'])==0)
+            {	
+        header('location:index.php');
+        }
+        else{
+            if(isset($_GET['del']))
+                {
+                    $id=$_GET['del'];
+
+                    $sql = "delete from testimonial WHERE id=:id";
+                    $query = $dbh->prepare($sql);
+                    $query -> bindParam(':id',$id, PDO::PARAM_STR);
+                    $query -> execute();                
+
+                    $msg="Data Deleted successfully";
+                }
+
+                    
+
+?>
+
+        <!DOCTYPE html>
+        <html>
+
+
+        <?php
+        require_once "public/config/header.php";
+        ?>
+
+<body>
+
+    <div id="wrapper">
+        <?php
+        require_once "public/config/left-sidebar.php";
+        ?>
+            
+            <div id="page-wrapper" class="gray-bg dashbard-1">
+                <div class="row border-bottom">
+            
+                <?php
+                require_once "public/config/topbar.php";
+                ?>
+                            
+                </div>
+                <div class="row dashboard-header">
+                <div class="panel-heading"style='padding:0;'><h2 class="page-title">Salary List</h2></div>
+				</div>
+            <div class="row">
+                       
+                <div class="col-lg-12">
+
+                    <h1><a class="btn btn-md btn-primary" href="#add" data-target="#add" data-toggle="modal" style="color:#fff;" class="small-box-footer"><i class="glyphicon glyphicon-plus text-blue"></i></a></h1>
+                  <!-- Zero Configuration Table -->
+				<div class="panel panel-default">
+                <div class="panel-heading">List Users</div>
+							<div class="panel-body">
+											<?php if($error){?><div class="errorWrap" id="msgshow"><?php echo htmlentities($error); ?> </div><?php } 
+								else if($msg){?><div class="succWrap" id="msgshow"><?php echo htmlentities($msg); ?> </div><?php }?>
+								<table id="zctb" class="display table table-striped table-bordered table-hover" cellspacing="0" width="100%">
+									<thead>
+										<tr>
+										<th>#</th>
+                                                <th>Type</th>
+                                                <th>Name</th>
+                                                <th>Amount</th>
+                                                <th>Employee</th>
+												<th>Description</th>										
+                                                <th>Eligibility</th>
+                                                <th>method</th>
+                                                <th>allowance</th>
+                                                <th>Date</th>
+                                                <th>Add Parameters</th>
+                                                <th>Action</th>	
+										</tr>
+									</thead>
+									
+									<tbody>
+
+										<?php $sql = "SELECT * from salary";
+										$query = $dbh -> prepare($sql);
+										$query->execute();
+										$results=$query->fetchAll(PDO::FETCH_OBJ);
+										$cnt=1;
+										if($query->rowCount() > 0)
+										{
+										foreach($results as $result)
+										{				?>	
+										<tr>
+											<td><?php echo htmlentities($cnt);?></td>
+										    <td><?php echo htmlentities($result->type);?></td>
+                                            <td><?php echo htmlentities($result->name);?></td>
+                                            <td><?php echo htmlentities($result->amount);?></td>
+                                            <td><?php echo htmlentities($result->amount);?></td>
+                                            <td><?php echo htmlentities($result->description);?></td>
+                                            <td><?php echo htmlentities($result->eligibility);?></td>
+                                            <td><?php echo htmlentities($result->method);?></td>
+                                            <td><?php echo htmlentities($result->allowance);?></td>
+                                            <td><?php echo htmlentities($result->date);?></td>
+                                            <td><?php echo htmlentities($result->add_parameter);?></td>
+
+																						
+											<!-- Action Button Start -->
+                                            <td>
+                                            <a data-toggle="modal" href="salaryedit.php?s=<?php echo $result->id;?>" data-target="#MyModal" data-backdrop="static">&nbsp;
+                                            <i class="fa fa-pencil"></i></a>&nbsp;&nbsp;
+                                            <div class="modal fade" id="MyModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                                    <div class="modal-dialog model-sm">
+                                                        <div class="modal-content"> </div>
+                                                    </div>
+                                            </div>
+
+                                            <a href="buildinglist.php?del=<?php echo $result->sn;?>"
+                                                onclick="return confirm('Do you want to Delete');"><i
+                                                    class="fa fa-trash" style="color:red"></i></a>&nbsp;&nbsp;
+                                        </td>
+
+                                        <!-- Action Button End -->
+
+										</tr>
+										<?php $cnt=$cnt+1; }} ?>
+										
+									</tbody>
+								</table>
+                            </div>
+                            <div id="add" class="modal fade in" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+  <div class="modal-dialog">
+    <div class="modal-content" style="height:auto">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">×</span></button>
+                <h4 class="modal-title">Add New Product</h4>
+              </div>
+              <div class="modal-body">
+
+                            <form action="salary.php" method="POST" class="forma">
+                 <p>
+                        <select name="type">
+                        <option selected>Salary</option>
+                        <option >Allowance</option>
+                        <option >Bonus</option>  
+                        </select>
+                    </p>
+					<p>
+                        <label for="full_name">Name</label>
+                        <input type="text" name="name">
+                    </p>
+                    <p>
+                        <label for="amount">Amount Payable</label>
+                        <input type="text" name="amount" value="">
+                    </p>
+                    <p>
+                        <select name="category">
+                        <option disabled selected>Employee status</option>
+                        <option >contract</option>
+                        <option >permanent</option>  
+                        </select>
+                    </p>
+
+                    <p>
+                        <label for="full_name">Description</label>
+                        <input type="text" name="description" value="">
+                    </p>
+
+                    <p>
+                        <label for="full_name">Eligibity</label>
+                        <input type="text" name="eligibility" value="">
+                    </p>
+
+                    <p>
+                        <label for="amount">Allowance</label>
+                        <input type="text" name="allowance" value="">
+                    </p>
+                    <p>
+                        <label for="amount">Method</label>
+                        <input type="text" name="method" value="">
+                    </p>
+					<p>
+                        <label for="date">Date</label>
+                        <input type="date" name="date" value="2017-06-01">
+                    </p>
+					<p>
+                        <label for="add_parameters">Add Parameter</label>
+                        <input type="textarea" name="add_parameters" value="">
+                    </p>
+                    
+                    <p>
+                        <button type="submit" name="submit">
+                            Submit
+                        </button>
+                    </p>
+
+                </form>
+                    
+            </div>
+            <div class="modal-footer">
+
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+      
+        </div><!--end of modal-dialog-->
+ </div>
+						</div>	
+
+                </div>
+                   
+                <!---
+                <div class="col-lg-4">
+                        <?php
+            //    require_once "public/config/right-sidebar.php";
+                ?>
+
+                            </div>
+                                                    -->
+            </div>
+
+         
+                        
+
+
+
+                        
+                    
+                </div>
+
+                
+            </div>
+            
+        </div>
+       
+        <?php
+                require_once "public/config/footer.php";
+                ?>
+
+    </body>
+    
+    <!-- Mirrored from webapplayers.com/inspinia_admin-v2.6.1/ by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 26 Sep 2016 02:26:53 GMT -->
+    </html>
+    
+    <?php } ?>
