065a314d3055bdbd6c265bfdd7b097c0bef60579
Abdullahi Temidayo Jimoh <abdullahij951@gmail.com>
2020-08-31 23:46:08 +0100

    Update header.php

--- admin/public/config/header.php
+++ admin/public/config/header.php
@@ -1,20 +1,20 @@
 <head>
-    
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    
+
     <title>DUFMA | Dashboard</title>
-    
+
     <link href="public/css/bootstrap.min.css" rel="stylesheet">
     <link href="public/font-awesome/css/font-awesome.css" rel="stylesheet">
 
     <!-- Toastr style -->
     <link href="public/css/plugins/toastr/toastr.min.css" rel="stylesheet">
-    
+
     <link href="public/css/plugins/dataTables/datatables.min.css" rel="stylesheet">
     <link href="public/css/plugins/jasny/jasny-bootstrap.min.css" rel="stylesheet">
-    
+
     <link href="public/css/animate.css" rel="stylesheet">
     <link href="public/css/style.css" rel="stylesheet">
     <link href="public/css/personal.css" rel="stylesheet">
@@ -24,24 +24,35 @@
 
 
     <!-- Icon style -->
-    <link rel="icon" type="image/png" sizes="192x192"  href="public/css/fav-icons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="public/css/fav-icons/android-icon-192x192.png">
     <link rel="icon" type="image/png" sizes="32x32" href="public/css/fav-icons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="96x96" href="public/css/fav-icons/favicon-96x96.png">
     <link rel="icon" type="image/png" sizes="16x16" href="public/css/fav-icons/favicon-16x16.png">
     <link href="public/css/all.css" rel="stylesheet">
-    
+
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/weather-icons/2.0.5/css/weather-icons.min.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">
     <!-- <link rel="stylesheet" href="public/css/boot.css" /> -->
+
+    <!-- tooltips -->
     
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    
+    <script>
+    $(document).ready(function() {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+    </script>
     <!-- Map -->
     <style>
     #map {
         height: 100%;
     }
-</style>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    
+    </style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
 </head>
