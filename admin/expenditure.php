f1269c1e12d1e9d55eeb9091efd3b8dabf200a95
Abdullahi Temidayo Jimoh <abdullahij951@gmail.com>
2020-09-01 14:56:05 +0100

    Update expenditure.php

--- admin/expenditure.php
+++ admin/expenditure.php
@@ -88,7 +88,7 @@
 													<div class="stat-panel-title text-uppercase">Raw Materials</div>
 												</div>
 											</div>
-											<a href="#" class="block-anchor panel-footer text-center">Full Detail &nbsp; <i class="fa fa-arrow-right"></i></a>
+											<a href="rawmaterials.php" class="block-anchor panel-footer text-center">Full Detail &nbsp; <i class="fa fa-arrow-right"></i></a>
 										</div>
 									</div>
                                     <div class="col-md-3">
