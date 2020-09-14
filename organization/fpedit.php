6db983bd9752b6b2daa4e919ae97f99cd0195778
Abdullahi Temidayo Jimoh <abdullahij951@gmail.com>
2020-09-14 20:23:49 +0100

    Update fpedit.php

--- organization/fpedit.php
+++ organization/fpedit.php
@@ -24,7 +24,7 @@ if (strlen($_SESSION['alogin']) == 0) {
 
         <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-            <h4 class="modal-title">Edit Building</h4>
+            <h4 class="modal-title">Edit Fields/Pens</h4>
         </div>
         <div class="modal-body">
 
