cadfb1379953ce72868bb59845a5f18f328b0e05
Abdullahi Temidayo Jimoh <abdullahij951@gmail.com>
2020-09-20 16:58:57 +0100

    Update landlist.php

--- organization/landlist.php
+++ organization/landlist.php
@@ -94,7 +94,7 @@ if (strlen($_SESSION['alogin']) == 0) {
 
                                     <tbody>
 
-                                        <?php $sql = "SELECT * from `locations` Where org_id=:org_id";
+                                        <?php $sql = "SELECT * from `locations` WHERE org_id=:org_id";
                                         $query = $dbh->prepare($sql);
                                         $query->bindParam(':org_id', $_SESSION['id'], PDO::PARAM_STR);
                                         $query->execute();
