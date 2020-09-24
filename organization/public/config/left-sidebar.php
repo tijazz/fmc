733a36298cb5ca2f89ba6dbfda3241d18b30062e
Abdullahi Temidayo Jimoh <abdullahij951@gmail.com>
2020-09-24 14:13:07 +0100

    Update left-sidebar.php

--- organization/public/config/left-sidebar.php
+++ organization/public/config/left-sidebar.php
@@ -70,8 +70,8 @@
                 <ul class="nav nav-second-level">
                     <li><a href="na.php">Landing Summary Page</a></li>
                     <li><a href="tangible-asset.php">Procurement</a></li>
-                    <li><a href="na.php">Product Management</a></li>
-                    <li><a href="na.php">Ware Housing</a></li>
+                    <li><a href="productlist.php">Product Management</a></li>
+                    <li><a href="warehouse.php">Ware Housing</a></li>
                     <li><a href="na.php">Input Analysis</a></li>
                     <li><a href="na.php">output</a></li>
                     <li><a href="na.php">Tracking and Report</a></li>
