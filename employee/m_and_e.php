aed81ff8035c965243154a2ffb613965bfcfd83a
Abdullahi Temidayo Jimoh <abdullahij951@gmail.com>
2020-10-01 15:17:21 +0100

    Update m_and_e.php

--- employee/m_and_e.php
+++ employee/m_and_e.php
@@ -170,7 +170,6 @@ if (strlen($_SESSION['alogin']) == 0) {
                             <div class="search-employee">
                                 <div class="search_employee">
                                     <input type="text" name="search_employee" id="" placeholder="Search Employees">
-                                    <?xml version="1.0" encoding="utf-8"?>
                                     <!-- Generator: Adobe Illustrator 17.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
                                     <!DOCTYPE svg
                                         PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
