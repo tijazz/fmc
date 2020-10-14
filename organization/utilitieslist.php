8ea15120f23758bf5742afc0132385916efed357
Abdullahi Temidayo Jimoh <abdullahij951@gmail.com>
2020-10-14 23:01:34 +0100

    Update utilitieslist.php

--- organization/utilitieslist.php
+++ organization/utilitieslist.php
@@ -1,202 +1,210 @@
 <?php
-        session_start();
-     
-        error_reporting(0);
-        $error="";
-        $msg="";
-        include('includes/config.php');
-        if(strlen($_SESSION['alogin'])==0)
-            {	
-        header('location:index.php');
-        }
-        else{
-            if(isset($_GET['del']))
-                {
-                    $id=$_GET['del'];
+session_start();
 
-                    $sql = "delete from testimonial WHERE id=:id";
-                    $query = $dbh->prepare($sql);
-                    $query -> bindParam(':id',$id, PDO::PARAM_STR);
-                    $query -> execute();                
+error_reporting(0);
+$error = "";
+$msg = "";
+include('includes/config.php');
+if (strlen($_SESSION['alogin']) == 0) {
+    header('location:index.php');
+} else {
+    if (isset($_GET['del'])) {
+        $id = $_GET['del'];
 
-                    $msg="Data Deleted successfully";
-                }
+        $sql = "delete from utilities WHERE id=:id";
+        $query = $dbh->prepare($sql);
+        $query->bindParam(':id', $id, PDO::PARAM_STR);
+        $query->execute();
 
-                    
+        $msg = "Data Deleted successfully";
+    }
+
+
 
 ?>
 
-        <!DOCTYPE html>
-        <html>
+    <!DOCTYPE html>
+    <html>
 
 
-        <?php
-        require_once "public/config/header.php";
-        ?>
+    <?php
+    require_once "public/config/header.php";
+    ?>
 
-<body>
+    <body>
 
-    <div id="wrapper">
-        <?php
-        require_once "public/config/left-sidebar.php";
-        ?>
-            
+        <div id="wrapper">
+            <?php
+            require_once "public/config/left-sidebar.php";
+            ?>
+
             <div id="page-wrapper" class="gray-bg dashbard-1">
                 <div class="row border-bottom">
-            
-                <?php
-                require_once "public/config/topbar.php";
-                ?>
-                            
+
+                    <?php
+                    require_once "public/config/topbar.php";
+                    ?>
+
                 </div>
                 <div class="row dashboard-header">
-                <div class="panel-heading" style='padding:0;'><h2 class="page-title">Manage Utilities</h2></div>
-				</div>
-            <div class="row">
-                       
-                <div class="col-lg-12">
+                    <div class="panel-heading" style='padding:0;'>
+                        <h2 class="page-title">Manage Utilities</h2>
+                    </div>
+                </div>
+                <div class="row">
 
-                    <h1><a class="btn btn-lg btn-md btn-primary" href="#add" data-target="#add" data-toggle="modal" style="color:#fff;" class="small-box-footer"><i class="glyphicon glyphicon-plus text-blue"></i></a></h1>
-                  <!-- Zero Configuration Table -->
-				<div class="panel panel-default">
-                <div class="panel-heading">List Users</div>
-							<div class="panel-body">
-											<?php if($error){?><div class="errorWrap" id="msgshow"><?php echo htmlentities($error); ?> </div><?php } 
-								else if($msg){?><div class="succWrap" id="msgshow"><?php echo htmlentities($msg); ?> </div><?php }?>
-								<table id="zctb tablePreview" class="display table table-striped table-bordered table-hover" cellspacing="0" width="100%">
-									<thead>
-										<tr>
-										<th>#</th>
-                                                <th>Serial number</th>
-                                                <th>Name</th>
-                                                <th>Description</th>
-                                                <th>Price</th>
-                                                <th>Date</th>
-                                                <th>Add Parameter</th>
-                                                <th>Action</th>	
-										</tr>
-									</thead>
-									
-									<tbody>
+                    <div class="col-lg-12">
 
-										<?php $sql = "SELECT * from `utilities`";
-										$query = $dbh -> prepare($sql);
-										$query->execute();
-										$results=$query->fetchAll(PDO::FETCH_OBJ);
-										$cnt=1;
-										if($query->rowCount() > 0)
-										{
-										foreach($results as $result)
-										{				?>	
-										<tr>
-											<td><?php echo htmlentities($cnt);?></td>
-										    <td><?php echo htmlentities($result->sn);?></td>
-                                            <td><?php echo htmlentities($result->name);?></td>
-                                            <td><?php echo htmlentities($result->description);?></td>
-                                            <td><?php echo htmlentities($result->amount);?></td>
-                                            <td><?php echo htmlentities($result->date);?></td>
-                                            <td><?php echo htmlentities($result->add_parameter);?></td>
+                        <!-- button style Start -->
+                        <div class="navbar">
+                            <div class="container-fluid" style="padding-left:7px;">
+                                <h1 class="nav navbar-nav">
+                                    <a class="btn btn-md btn-primary" href="#add" data-target="#add" data-toggle="modal" style="color:#fff;" class="small-box-footer"><i class="glyphicon glyphicon-plus text-blue"></i> Add Utility</a>
+                                </h1>
 
-																						
-											<td>
-											<a href="edit-testimo.php?edit=<?php echo $result->id;?>" onclick="return confirm('Do you want to Edit');">&nbsp; <i class="fa fa-pencil"></i></a>&nbsp;&nbsp;
-											<a href="testimolist.php?del=<?php echo $result->id;?>;?>" onclick="return confirm('Do you want to Delete');"><i class="fa fa-trash" style="color:red"></i></a>&nbsp;&nbsp;
-											</td>
-										</tr>
-										<?php $cnt=$cnt+1; }} ?>
-										
-									</tbody>
-								</table>
+
+                            </div>
+                        </div>
+                        <!-- button style End -->
+
+
+                        <!-- Zero Configuration Table -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading">List Users</div>
+                            <div class="panel-body">
+                                <?php if ($error) { ?><div class="errorWrap" id="msgshow"><?php echo htmlentities($error); ?> </div><?php } else if ($msg) { ?><div class="succWrap" id="msgshow"><?php echo htmlentities($msg); ?> </div><?php } ?>
+                                <table id="zctb tablePreview" class="display table table-striped table-bordered table-hover" cellspacing="0" width="100%">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Name</th>
+                                            <th>Description</th>
+                                            <th>Price</th>
+                                            <th>Date</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+
+                                        <?php $sql = "SELECT * from `utilities` WHERE org_id = :org_id";
+                                        $query = $dbh->prepare($sql);
+                                        $query->bindParam(':org_id', $_SESSION['org_id'], PDO::PARAM_STR);
+                                        $query->execute();
+                                        $results = $query->fetchAll(PDO::FETCH_OBJ);
+                                        $cnt = 1;
+                                        if ($query->rowCount() > 0) {
+                                            foreach ($results as $result) {                ?>
+                                                <tr>
+                                                    <td><?php echo htmlentities($cnt); ?></td>
+                                                    <td><?php echo htmlentities($result->name); ?></td>
+                                                    <td><?php echo htmlentities($result->description); ?></td>
+                                                    <td><?php echo htmlentities($result->amount); ?></td>
+                                                    <td><?php echo htmlentities($result->date); ?></td>
+
+
+
+                                                    <!-- Action Button Start -->
+                                                    <td>
+                                                        <a data-toggle="modal" href="utilitiesedit.php?edit=<?php echo $result->id; ?>" data-target="#MyModal" data-backdrop="static">&nbsp;
+                                                            <i class="fa fa-pencil"></i></a>&nbsp;&nbsp;
+                                                        <div class="modal fade" id="MyModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                                            <div class="modal-dialog model-sm">
+                                                                <div class="modal-content"> </div>
+                                                            </div>
+                                                        </div>
+
+                                                        <a href="utilities.php?del=<?php echo $result->id; ?>;?>" onclick="return confirm('Do you want to Delete');"><i class="fa fa-trash" style="color:red"></i></a>&nbsp;&nbsp;
+                                                    </td>
+
+                                                    <!-- Action Button End -->
+                                                </tr>
+                                        <?php $cnt = $cnt + 1;
+                                            }
+                                        } ?>
+
+                                    </tbody>
+                                </table>
                             </div>
                             <div id="add" class="modal fade in" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
-  <div class="modal-dialog">
-    <div class="modal-content" style="height:auto">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">×</span></button>
-                <h4 class="modal-title">Add New Product</h4>
-              </div>
-              <div class="modal-body">
-              <form action="utilities.php" method="POST" class="forma">
-                    
-					<p>
-                        <label for="sn">Serial Number</label>
-                        <input type="text" name="sn">
-                    </p>
-					<p>
-                        <label for="full_name">Name of Utility</label>
-                        <input type="text" name="name">
-                    </p>
-                    <p>
-                        <label for="full_name">Description</label>
-                        <input type="text" name="description" value="">
-                    </p>
+                                <div class="modal-dialog">
+                                    <div class="modal-content" style="height:auto">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">×</span></button>
+                                            <h4 class="modal-title">Add New Utility</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form action="utilities.php" method="POST" class="forma">
 
-                    <p>
-                        <label for="amount">Amount of Utility</label>
-                        <input type="text" name="amount" value="">
-                    </p>
-					<p>
-                        <label for="date">Date of Expiry</label>
-                        <input type="date" name="date" value="2017-06-01">
-                    </p>
-					
-                        <label for="add_parameters">Add Parameter</label>
-                        <input type="textarea" name="add_parameters" value="">
-                    </p>
-                    
-                    <p>
-                        <button type="submit" name="submit">
-                            Submit
-                        </button>
-                    </p>
+                                                <p>
+                                                    <label for="full_name">Name of Utility</label>
+                                                    <input type="text" name="name">
+                                                </p>
+                                                <p>
+                                                    <label for="full_name">Description</label>
+                                                    <input type="text" name="description" value="">
+                                                </p>
 
-                </form>
+                                                <p>
+                                                    <label for="amount">Amount of Utility</label>
+                                                    <input type="text" name="amount" value="">
+                                                </p>
 
-                    
-            </div>
-            <div class="modal-footer">
+                                                <p>
+                                                    <button type="submit" name="submit">
+                                                        Submit
+                                                    </button>
+                                                </p>
 
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-              </div>
-      
-        </div><!--end of modal-dialog-->
- </div>
-						</div>	
+                                            </form>
 
-                </div>
-                   
-                <!---
+
+                                        </div>
+                                        <div class="modal-footer">
+
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                        </div>
+
+                                    </div>
+                                    <!--end of modal-dialog-->
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <!---
                 <div class="col-lg-4">
                         <?php
-            //    require_once "public/config/right-sidebar.php";
-                ?>
+                        //    require_once "public/config/right-sidebar.php";
+                        ?>
 
                             </div>
                                                     -->
-            </div>
-
-         
-                        
+                    </div>
 
 
 
-                        
-                    
+
+
+
+
+
                 </div>
 
-                
+
             </div>
-            
+
         </div>
-       
+
         <?php
-                require_once "public/config/footer.php";
-                ?>
+        require_once "public/config/footer.php";
+        ?>
 
     </body>
-    
+
     <!-- Mirrored from webapplayers.com/inspinia_admin-v2.6.1/ by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 26 Sep 2016 02:26:53 GMT -->
+
     </html>
-    
-    <?php } ?>
+
+<?php } ?>
