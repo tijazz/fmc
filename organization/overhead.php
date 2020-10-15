9335f3810c71faa6bea81f107cb099c71ead4745
Abdullahi Temidayo Jimoh <abdullahij951@gmail.com>
2020-10-15 23:26:38 +0100

    Update overhead.php

--- organization/overhead.php
+++ organization/overhead.php
@@ -245,10 +245,10 @@ if (strlen($_SESSION['alogin']) == 0) {
                                                         </metadata>
                                                     </svg>
                                                     <div class="stat-panel-number h1 "><?php echo htmlentities($query); ?></div>
-                                                    <div class="stat-panel-title text-uppercase">Depreciation Cost</div>
+                                                    <div class="stat-panel-title text-uppercase">Purchases</div>
                                                 </div>
                                             </div>
-                                            <a href="#" class="block-anchor panel-footer text-center">Full Detail &nbsp; <i class="fa fa-arrow-right"></i></a>
+                                            <a href="purchaseslist.php" class="block-anchor panel-footer text-center">Full Detail &nbsp; <i class="fa fa-arrow-right"></i></a>
                                         </div>
                                     </div>
                                     <div class="col-md-3">
