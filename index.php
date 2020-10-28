df56cf9229c19edd94ee22a0d4fb989d9df03620
Fawaz <abdulsalamfawaz4@gmail.com>
2020-10-28 08:10:59 +0100

    Added home page

---
+++ index.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DUFMA</title>
+    <link rel="stylesheet" href="public/css/presets.css">
+    <link rel="stylesheet" href="public/css/landing.css">
+    <link rel="shortcut icon" href="public/images/favicon.png" type="image/x-icon">
+</head>
+
+<body>
+    <nav class="main_nav flex a_cent">
+        <div class="logo">
+            <img src="public/images/favicon.png" alt="">
+        </div>
+        <ul class="right_nav">
+            <li><a href="#">Home</a></li>
+            <li class="parent"><a href="#">Solutions
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                        <path
+                            d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z" />
+                        </svg>
+                </a>
+                <ul class="child">
+                    <li><a href="#">Media</a></li>
+                    <li><a href="#">Case Studies</a></li>
+                    <li><a href="#">Webinars</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Request a Demo</a></li>
+            <li class="parent"><a href="#">Contact us
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                        <path
+                            d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z" />
+                        </svg>
+                </a>
+                <ul class="child">
+                    <li><a href="#">Careers</a></li>
+                    <li><a href="#">Services</a></li>
+                </ul>
+            </li>
+            <li><a href="gen_index.html">Login</a></li>
+        </ul>
+        <li class="mobile_ham flex column">
+            <span></span>
+            <span></span>
+        </li>
+    </nav>
+    <ul class="mobile_nav">
+        <li><a href="#">Home</a></li>
+        <li class="parent1 parent"><a href="#">Solutions
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                    <path
+                        d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z" />
+                    </svg>
+            </a>
+
+        </li>
+        <ul class="child1">
+            <li><a href="#">Media</a></li>
+            <li><a href="#">Case Studies</a></li>
+            <li><a href="#">Webinars</a></li>
+        </ul>
+        <li><a href="#">Request A Demo</a></li>
+        <li class="parent2 parent"><a href="#">Contact Us
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                    <path
+                        d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z" />
+                    </svg>
+            </a>
+        </li>
+        <ul class="child2">
+            <li><a href="#">Media</a></li>
+            <li><a href="#">Case Studies</a></li>
+            <li><a href="#">Webinars</a></li>
+        </ul>
+        <li><a href="gen_index.html">Login</a></li>
+    </ul>
+    <div class="hero">
+        <img src="public/images/slide1.jpg" alt="" class="active">
+        <img src="public/images/slide2.jpg" alt="">
+        <img src="public/images/slide3.jpg" alt="">
+        <div class="text1 text">
+            <h1>Lorem</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat!</p>
+        </div>
+        <div class="text2 text">
+            <h1>Lorem1</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat!</p>
+        </div>
+        <div class="text3 text">
+            <h1>Lorem2</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat!</p>
+        </div>
+    </div>
+    <div class="bubbles">
+        <span class="active"></span>
+        <span></span>
+        <span></span>
+    </div>
+    <div class="info">
+        <div class="text">
+            <h2>Dufma
+                <span></span>
+            </h2>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio fugiat exercitationem eaque, aut ratione
+                delectus labore officiis totam ipsa, aliquam quam, laudantium accusantium corrupti debitis alias
+                possimus recusandae repellat cum dolorem ducimus consequatur aspernatur! Quo esse cum, voluptatem
+                adipisci sed tenetur corporis eius aut modi hic veritatis magnam consequatur natus saepe mollitia illo
+                ipsam ab id molestiae officia provident recusandae temporibus. Fugiat vitae dolorem aut, consequuntur
+                officiis et commodi qui, harum saepe voluptatibus id! Odio molestiae quas, velit esse repellendus quo
+                fuga exercitationem mollitia aliquam commodi eius ea rerum in non sit omnis temporibus odit, ut eum.
+                Tenetur, itaque explicabo.
+            </p>
+        </div>
+        <div class="illustr">
+            <img src="public/images/illustr.jpg" alt="">
+        </div>
+    </div>
+    <div class="demo_req">
+        <h2>Request A Demo</h2>
+        <div class="demo_req_content">
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quas omnis voluptatem culpa
+                    similique molestias tenetur inventore, rerum quia aperiam.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quas omnis voluptatem culpa
+                    similique molestias tenetur inventore, rerum quia aperiam.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quas omnis voluptatem culpa
+                    similique molestias tenetur inventore, rerum quia aperiam.</p>
+                <img src="public/images/8400.jpg" alt="Image" class="small">
+            </div>
+            <form class="form">
+                <input type="email" name="email" id="email" placeholder="Email">
+                <input type="text" name="phone" id="phone" placeholder="Phone">
+                <textarea name="message" id="message" placeholder='Message Here' cols="30" rows="10"></textarea>
+                <input type="submit" value="Submit">
+            </form>
+        </div>
+    </div>
+    <div class="solutions">
+        <h2>Solutions
+            <span></span>
+        </h2>
+        <div class="cards">
+            <div class="card">
+                <div class="card_image flex a_cent j_cent">
+                    <img src="public/images/ai-brain.svg" alt="">
+                </div>
+                <div class="card_text">
+                    <p>ML/AI</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card_image flex a_cent j_cent">
+                    <img src="public/images/blockchain.svg" alt="">
+                </div>
+                <div class="card_text">
+                    <p>Blockchain</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card_image flex a_cent j_cent">
+                    <img src="public/images/farmer.svg" alt="">
+                </div>
+                <div class="card_text">
+                    <p>Profiling</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card_image flex a_cent j_cent">
+                    <img src="public/images/fertilizer.svg" alt="">
+                </div>
+                <div class="card_text">
+                    <p>Procurement</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card_image flex a_cent j_cent">
+                    <img src="public/images/satellite.svg" alt="">
+                </div>
+                <div class="card_text">
+                    <p>Remote</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card_image flex a_cent j_cent">
+                    <img src="public/images/smartphone.svg" alt="">
+                </div>
+                <div class="card_text">
+                    <p>Flexible</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card_image flex a_cent j_cent">
+                    <img src="public/images/tractor.svg" alt="">
+                </div>
+                <div class="card_text">
+                    <p>Supply</p>
+                    <p>Chain</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card_image flex a_cent j_cent">
+                    <img src="public/images/transportation.svg" alt="">
+                </div>
+                <div class="card_text">
+                    <p>Risk Management</p>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+    <div class="newsletter flex a_cent j_cent">
+        <div>
+            <h1>Never Miss Out on Any Updates</h1>
+            <div class="news_form">
+                <p>Subscribe to Our Newsletter</p>
+                <form action="" class="flex">
+                    <input type="email" name="Email" placeholder="Email" id="">
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+<script src="public/js/gsap.min.js"></script>
+<script src="public/js/landing.js"></script>
+
+</html>
