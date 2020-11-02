8d7f5724897c8efe10d8bb5af618946013ca868e
Abdullahi Temidayo Jimoh <abdullahij951@gmail.com>
2020-11-02 15:51:37 +0100

    Facility Management updated

--- organization/ftlist.php
+++ organization/ftlist.php
@@ -9,6 +9,37 @@ if (strlen($_SESSION['alogin']) == 0) {
     header('location:index.php');
 } else {
 
+    if (isset($_POST['edit'])) {
+        $id = $_POST['edit'];
+        $capacity = $_POST['capacity'];
+        $type = $_POST['type'];
+        $purpose = $_POST['purpose'];
+        $status = $_POST['status'];
+        $utilization = $_POST['utilization'];
+        $start_season = $_POST['start_season'];
+        $end_season = $_POST['end_season'];
+        $ownership = $_POST['ownership'];
+        $manager = $_POST['manager'];
+        $fallow = $_POST['fallow'];
+
+        $sql = "UPDATE `building` SET `capacity`=:capacity,`type`=:type,`purpose`=:purpose,`status`=:status,`utilization`=:utilization,`start_season`=:start_season,`end_season`=:end_season,`ownership`=:ownership,`fallow`=:fallow,`manager`=:manager WHERE id=(:id)";
+        $query = $dbh->prepare($sql);
+        $query->bindParam(':capacity', $capacity, PDO::PARAM_STR);
+        $query->bindParam(':type', $type, PDO::PARAM_STR);
+        $query->bindParam(':purpose', $purpose, PDO::PARAM_STR);
+        $query->bindParam(':status', $status, PDO::PARAM_STR);
+        $query->bindParam(':utilization', $utilization, PDO::PARAM_STR);
+        $query->bindParam(':start_season', $start_season, PDO::PARAM_STR);
+        $query->bindParam(':end_season', $end_season, PDO::PARAM_STR);
+        $query->bindParam(':ownership', $ownership, PDO::PARAM_STR);
+        $query->bindParam(':manager', $manager, PDO::PARAM_STR);
+        $query->bindParam(':fallow', $fallow, PDO::PARAM_STR);
+        $query->bindValue(':id', $id, PDO::PARAM_STR);
+        $query->execute();
+        $msg = "Rent Updated Successfully";
+        // echo var_dump($_POST);
+        header('location:ftlist.php');
+    }
 
 
 ?>
@@ -105,13 +136,116 @@ if (strlen($_SESSION['alogin']) == 0) {
 
                                                     <!-- Action Button Start -->
                                                     <td>
-                                                        <a data-toggle="modal" href="ftedit.php?s=<?php echo $result->sn; ?>" data-target="#MyModal" data-backdrop="static">&nbsp;
+                                                        <a data-toggle="modal" href="#edit<?= $cnt ?>" data-toggle="modal" data-target="#edit<?= $cnt ?>">&nbsp;
                                                             <i class="fa fa-pencil"></i></a>&nbsp;&nbsp;
-                                                        <div class="modal fade" id="MyModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                                                            <div class="modal-dialog model-sm">
-                                                                <div class="modal-content"> </div>
+
+                                                        <div class="modal fade" id="edit<?= $cnt ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                            <div class="modal-dialog">
+                                                                <div class="modal-content" style="height:auto">
+                                                                    <div class="modal-header">
+                                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                            <span aria-hidden="true">×</span></button>
+                                                                        <h4 class="modal-title">Add Detail</h4>
+                                                                    </div>
+                                                                    <div class="modal-body">
+                                                                        <form action="ftlist.php" method="POST" class="forma">
+                                                                            <p>
+                                                                                <label for="name">Name</label>
+                                                                                <input type="text" name="name" value="<?php echo ($result->name); ?>" disabled>
+                                                                            </p>
+
+
+                                                                            <p>
+                                                                                <label for="size">Size</label>
+                                                                                <input type="text" name="size" value="<?php echo ($result->size); ?>" disabled>
+                                                                            </p>
+
+                                                                            <p>
+                                                                                <label for="latitude">Latitude</label>
+                                                                                <input type="text" name="latitude" value="<?php echo ($result->lat); ?>" disabled>
+                                                                            </p>
+
+                                                                            <p>
+                                                                                <label for="longitude">Longitude</label>
+                                                                                <input type="text" name="longitude" value="<?php echo ($result->lng); ?>" disabled>
+                                                                            </p>
+
+                                                                            <p>
+                                                                                <label for="capacity">Capacity</label>
+                                                                                <input type="text" name="capacity" value="<?php echo ($result->capacity); ?>">
+                                                                            </p>
+
+                                                                            <p>
+                                                                                <label for="type">Type</label>
+                                                                                <input type="text" name="type" value="<?php echo ($result->type); ?>">
+                                                                            </p>
+
+                                                                            <p>
+                                                                                <label for="purpose">Purpose</label>
+                                                                                <input type="text" name="purpose" value="<?php echo ($result->purpose); ?>">
+                                                                            </p>
+
+                                                                            <p>
+                                                                                <label for="status">Status</label>
+                                                                                <select name="status">
+                                                                                    <option value="full">Active</option>
+                                                                                    <option value="lease">Inactive</option>
+                                                                                </select>
+                                                                            </p>
+
+                                                                            <p>
+                                                                                <label for="utilization">Utilization</label>
+                                                                                <input type="text" name="utilization" value="<?php echo ($result->utilization); ?>">
+                                                                            </p>
+
+
+                                                                            <p>
+                                                                                <label for="start_season">Start Season</label>
+                                                                                <input type="date" name="start_season" value="<?php echo ($result->start_season); ?>">
+                                                                            </p>
+
+                                                                            <p>
+                                                                                <label for="end_season">End Season</label>
+                                                                                <input type="date" name="end_season" value="<?php echo ($result->end_season); ?>">
+                                                                            </p>
+
+                                                                            <p>
+                                                                                <label for="ownership">OwnerShip</label>
+                                                                                <select name="ownership">
+                                                                                    <option value="full">Full</option>
+                                                                                    <option value="lease">Lease</option>
+                                                                                    <option value="rent">Rent</option>
+                                                                                </select>
+                                                                            </p>
+
+                                                                            <p>
+                                                                                <label for="fallow">Fallow Period</label>
+                                                                                <input type="text" name="fallow" value="<?php echo ($result->fallow); ?>">
+                                                                            </p>
+
+                                                                            <p>
+                                                                                <label for="manager">Manager</label>
+                                                                                <input type="text" name="manager" value="">
+                                                                            </p>
+
+                                                                            <p>
+                                                                                <button type="submit" name="edit" value="<?php echo $result->id; ?>">
+                                                                                    Submit
+                                                                                </button>
+                                                                            </p>
+
+                                                                        </form>
+                                                                    </div>
+                                                                    <div class="modal-footer">
+
+                                                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                                    </div>
+
+                                                                </div>
+
                                                             </div>
                                                         </div>
+                                                        <!--end of modal-dialog-->
 
                                                     </td>
 
