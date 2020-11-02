2e23a060056b1aa48662076dc6cfd5680d628c40
Abdullahi Temidayo Jimoh <abdullahij951@gmail.com>
2020-11-02 10:33:44 +0100

    updated report

--- organization/penmonthlyreport.php
+++ organization/penmonthlyreport.php
@@ -113,10 +113,10 @@ if (strlen($_SESSION['alogin']) == 0) {
                                                     <td><?php
                                                         $s =  $result->manager == 0 ? "SELECT * FROM `organization` WHERE id = (:id)" : "SELECT * FROM `employee` WHERE id = (:id)";
                                                         $q = $dbh->prepare($s);
-                                                        $q->bindParam(':id', $result->manager, PDO::PARAM_STR);
+                                                        $result->manager == 0 ? $q->bindParam(':id', $result->org_id, PDO::PARAM_STR) : $q->bindParam(':id', $result->manager, PDO::PARAM_STR);
                                                         $q->execute();
                                                         $res = $q->fetch(PDO::FETCH_OBJ);
-                                                        echo htmlentities($result->manager == 0 ? $result->manager : $res->name);  ?></td>
+                                                        echo htmlentities($result->manager == 0 ? $res->username : $res->name);  ?></td>
                                                     <td>
                                                         <a data-toggle="modal" href="penmonthlyreportedit.php?s=<?php echo $result->id; ?>" data-target="#MyModal" data-backdrop="static">&nbsp;
                                                             <i class="fa fa-pencil"></i></a>&nbsp;&nbsp;
