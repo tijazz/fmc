043b129634b76c9915ee2589c1ac41de74b97385
Abdullahi Temidayo Jimoh <abdullahij951@gmail.com>
2020-11-03 11:13:28 +0100

    Update facilityweeklyreport.php

--- organization/facilityweeklyreport.php
+++ organization/facilityweeklyreport.php
@@ -19,8 +19,65 @@ if (strlen($_SESSION['alogin']) == 0) {
         $msg = "Data Deleted successfully";
         header('location:facilityweeklyreport.php');
     }
+    if (isset($_POST['submit'])) {
+
+        $user_id = $_SESSION['org_id'];
+        $org_id = $_SESSION['user_id'];
+        $week = $_POST['week'];
+        $hours = $_POST['hours'];
+        $name = $_POST['name'];
+        $activity = $_POST['activity'];
+        $activity_status = $_POST['activity_status'];
+        $field_status = $_POST['field_status'];
+        $manager = $_SESSION['user_id'];
+        $type = "facility";
 
 
+
+        $sql = "INSERT INTO `weeklyreport`(`user_id`, `org_id`, `week`, `name`, `hours`, `activity`, `activity_status`, `field_status`, `manager`, `type`) VALUES 
+        (:user_id, :org_id, :week, :name, :hours, :activity, :activity_status, :field_status, :manager, :type)";
+        $query = $dbh->prepare($sql);
+        $query->bindParam(':user_id', $user_id, PDO::PARAM_STR);
+        $query->bindParam(':org_id', $org_id, PDO::PARAM_STR);
+        $query->bindParam(':week', $week, PDO::PARAM_STR);
+        $query->bindParam(':name', $name, PDO::PARAM_STR);
+        $query->bindParam(':hours', $hours, PDO::PARAM_STR);
+        $query->bindParam(':activity', $activity, PDO::PARAM_STR);
+        $query->bindParam(':activity_status', $activity_status, PDO::PARAM_STR);
+        $query->bindParam(':field_status', $field_status, PDO::PARAM_STR);
+        $query->bindParam(':manager', $manager, PDO::PARAM_STR);
+        $query->bindParam(':type', $type, PDO::PARAM_STR);
+        $bind = $query->execute();
+        echo var_dump($bind);
+        header('location:facilityweeklyreport.php');
+    }
+
+    if (isset($_POST['edit'])) {
+        $id = $_POST['edit'];
+
+        $week = $_POST['week'];
+        $hours = $_POST['hours'];
+        $name = $_POST['name'];
+        $activity = $_POST['activity'];
+        $activity_status = $_POST['activity_status'];
+        $field_status = $_POST['field_status'];
+        $manager = $_SESSION['user_id'];
+        $type = "facility";
+
+        $sql = "UPDATE `weeklyreport` SET `week`=:week,`name`=:name,`hours`=:hours,`activity`=:activity,`activity_status`=:activity_status,`field_status`=:field_status WHERE id=(:id)";
+        $query = $dbh->prepare($sql);
+        $query->bindParam(':week', $week, PDO::PARAM_STR);
+        $query->bindParam(':name', $name, PDO::PARAM_STR);
+        $query->bindParam(':hours', $hours, PDO::PARAM_STR);
+        $query->bindParam(':activity', $activity, PDO::PARAM_STR);
+        $query->bindParam(':activity_status', $activity_status, PDO::PARAM_STR);
+        $query->bindParam(':field_status', $field_status, PDO::PARAM_STR);
+        $query->bindValue(':id', $id, PDO::PARAM_STR);
+        $query->execute();
+        $msg = "Rent Updated Successfully";
+
+        header('location:facilityweeklyreport.php');
+    }
 
 
 ?>
@@ -100,7 +157,7 @@ if (strlen($_SESSION['alogin']) == 0) {
                                                     <td><?php echo htmlentities($cnt); ?></td>
                                                     <td><?php echo htmlentities($result->week); ?></td>
                                                     <td><?php
-                                                        $s = "SELECT * FROM `building` WHERE sn = (:id) AND org_id = (:org_id)";
+                                                        $s = "SELECT * FROM `building` WHERE id = (:id) AND org_id = (:org_id)";
                                                         $q = $dbh->prepare($s);
                                                         $q->bindParam(':org_id', $_SESSION['org_id'], PDO::PARAM_STR);
                                                         $q->bindParam(':id', $result->name, PDO::PARAM_STR);
@@ -119,13 +176,87 @@ if (strlen($_SESSION['alogin']) == 0) {
                                                         $res = $q->fetch(PDO::FETCH_OBJ);
                                                         echo htmlentities($result->manager == 0 ? $res->username : $res->name);  ?></td>
                                                     <td>
-                                                        <a data-toggle="modal" href="fieldweeklyreportedit.php?s=<?php echo $result->id; ?>" data-target="#MyModal" data-backdrop="static">&nbsp;
+                                                        <a data-toggle="modal" href="#edit<?= $cnt ?>" data-toggle="modal" data-target="#edit<?= $cnt ?>">&nbsp;
                                                             <i class="fa fa-pencil"></i></a>&nbsp;&nbsp;
-                                                        <div class="modal fade" id="MyModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                                                            <div class="modal-dialog model-sm">
-                                                                <div class="modal-content"> </div>
+
+                                                        <div class="modal fade" id="edit<?= $cnt ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                            <div class="modal-dialog">
+                                                                <div class="modal-content" style="height:auto">
+                                                                    <div class="modal-header">
+                                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                            <span aria-hidden="true">×</span></button>
+                                                                        <h4 class="modal-title">Add Detail</h4>
+                                                                    </div>
+                                                                    <div class="modal-body">
+                                                                        <form action="facilityweeklyreport.php" method="POST" class="forma">
+                                                                            <p>
+                                                                                <label for="week">Week</label>
+                                                                                <input type="week" name="week" value="<?php echo $result->week ?>">
+                                                                            </p>
+
+                                                                            <p>
+                                                                                <label for="name">Facility</label>
+                                                                                <select name="name" id="">
+                                                                                    <?php
+                                                                                    $s = "SELECT * FROM `building` WHERE org_id = (:org_id)";
+                                                                                    $q = $dbh->prepare($s);
+                                                                                    $q->bindParam(':org_id', $_SESSION['org_id'], PDO::PARAM_STR);
+                                                                                    $q->execute();
+                                                                                    $res = $q->fetchAll(PDO::FETCH_OBJ);
+                                                                                    $cnt = 1;
+                                                                                    if ($q->rowCount() > 0) {
+                                                                                        foreach ($res as $re) {                ?>
+                                                                                            <option value="<?php echo htmlentities($re->id); ?>">
+                                                                                                <?php echo htmlentities($re->name); ?></option>
+                                                                                    <?php $cnt = $cnt + 1;
+                                                                                        }
+                                                                                    } ?>
+                                                                                </select>
+                                                                            </p>
+
+
+                                                                            <p>
+                                                                                <label for="hour">Hours</label>
+                                                                                <input type="number" name="hours" value="<?php echo $result->hours ?>">
+
+                                                                            </p>
+
+                                                                            <p>
+                                                                                <label for="activity">Activity</label>
+                                                                                <textarea name="activity" id="" cols="30" rows="10"><?php echo $result->activity ?></textarea>
+                                                                            </p>
+
+                                                                            <p>
+                                                                                <label for="activity_status">Activity Status</label>
+                                                                                <select name="activity_status" id="">
+                                                                                    <option value="Completed">Completed</option>
+                                                                                    <option value="On Going">On Going</option>
+                                                                                </select>
+                                                                            </p>
+
+                                                                            <p>
+                                                                                <label for="field_status">Field Status</label>
+                                                                                <input type="range" name="field_status" id="" min="1" max="5" step="1" value="<?php echo $result->field_status ?>">
+                                                                            </p>
+
+                                                                            <p>
+                                                                                <button type="submit" name="edit" value="<?php echo $result->id ?>">
+                                                                                    Submit
+                                                                                </button>
+                                                                            </p>
+
+                                                                        </form>
+                                                                    </div>
+                                                                    <div class="modal-footer">
+
+                                                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                                    </div>
+
+                                                                </div>
+
                                                             </div>
                                                         </div>
+                                                        <!--end of modal-dialog-->
 
                                                         <a href="facilityweeklyreport.php?del=<?php echo $result->id; ?>" onclick="return confirm('Do you want to Delete');"><i class="fa fa-trash" style="color:red"></i></a>&nbsp;&nbsp;
 
@@ -149,7 +280,7 @@ if (strlen($_SESSION['alogin']) == 0) {
                                             <h4 class="modal-title">Create Report</h4>
                                         </div>
                                         <div class="modal-body">
-                                            <form action="facilityweeklyreportform.php" method="POST" class="forma">
+                                            <form action="facilityweeklyreport.php" method="POST" class="forma">
                                                 <p>
                                                     <label for="week">Week</label>
                                                     <input type="week" name="week">
@@ -167,7 +298,7 @@ if (strlen($_SESSION['alogin']) == 0) {
                                                         $cnt = 1;
                                                         if ($query->rowCount() > 0) {
                                                             foreach ($results as $result) {                ?>
-                                                                <option value="<?php echo htmlentities($result->sn); ?>">
+                                                                <option value="<?php echo htmlentities($result->id); ?>">
                                                                     <?php echo htmlentities($result->name); ?></option>
                                                         <?php $cnt = $cnt + 1;
                                                             }
