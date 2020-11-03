da078362927981faa34b47cd4f0f32d286024404
Fawaz <abdulsalamfawaz4@gmail.com>
2020-11-03 23:57:10 +0100

    edited some styling master branch

--- public/js/notifications.js
+++ public/js/notifications.js
@@ -1,23 +1,8 @@
-let backdropd = document.querySelector('.backdrop');
 let notificationsd = document.querySelector('.notifications__')
-let notificationTriggerd= document.querySelector("#page-wrapper > div:nth-child(1) > nav > ul > li.dropdown.notifier");
-let notificationBelld = document.querySelector(".notification-bell");
-let notificationCountd = document.querySelector("#page-wrapper > div:nth-child(1) > nav > ul > li.dropdown.notifier > a > span")
-notificationTriggerd.addEventListener('click',() => {
-    backdropd.classList.add('shown')
-    notificationsd.classList.add('shown')
-    notificationBelld.classList.add('shaker')
-})
-
-backdropd.addEventListener('click',() => {
-    backdropd.classList.remove('shown')
-    notificationsd.classList.remove('shown')
-    notificationBelld.classList.remove('shaker')
-})
-
+notificationCountd = document.querySelector('.notifier a span')
 
 window.onload = () => {
-    var notificationListCount  = notificationsd.querySelectorAll('li').length
+    var notificationListCount  = document.querySelectorAll('.user_notification').length
     notificationCountd.innerHTML = notificationListCount
     
 }
