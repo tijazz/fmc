17452f576d32b23a5a5ded3f3b6d4b51a508c7e3
Fawaz <abdulsalamfawaz4@gmail.com>
2020-11-04 00:11:40 +0100

    notifications

--- public/js/notifications.js
+++ public/js/notifications.js
@@ -1,8 +1,17 @@
-let notificationsd = document.querySelector('.notifications__')
+let notificationsd = document.querySelector('.notifications_')
 notificationCountd = document.querySelector('.notifier a span')
+let notifier = document.querySelector('.notifier');
 
 window.onload = () => {
     var notificationListCount  = document.querySelectorAll('.user_notification').length
     notificationCountd.innerHTML = notificationListCount
+    if(notificationListCount == 0){
+        console.log(notificationCountd)
+        // notificationCountd.style.display = 'none'
+    }
     
 }
+
+notifier.addEventListener('click',() => {
+    notificationsd.classList.toggle('jq')
+})
