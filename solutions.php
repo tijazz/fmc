70ceb04d6d14f5d75aa4e04376ca7e2a1f0a763b
Fawaz <abdulsalamfawaz4@gmail.com>
2020-12-06 23:27:23 +0100

    set active section of nav for solutions.php

--- solutions.php
+++ solutions.php
@@ -20,8 +20,8 @@
                 </div>
             </a>
             <ul class="right_nav">
-                <li class="active"><a href="index.php">Home</a></li>
-                <li class="parent"><a href="solutions.php">Solutions
+                <li><a href="index.php">Home</a></li>
+                <li class="parent active "><a href="solutions.php">Solutions
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
                             <path
                                 d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z" />
